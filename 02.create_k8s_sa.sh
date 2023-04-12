68dcee04387f4658860d9a20a5ee146632b0d273
lomar92 <amarlojo@gmail.com>
2023-04-12 10:31:16 +0200

    no-secret

--- 02.create_k8s_sa.sh
+++ 02.create_k8s_sa.sh
@@ -8,6 +8,3 @@ kubectl delete -f CRB-vault-auth-service-account.yaml
 #create service account for k8s auth method
 #assign auth-delegator cluster role binding to Vault sa
 kubectl apply -f CRB-vault-auth-service-account.yaml
-
-#create secret for sa vault-auth -- required since k8s version 1.24+
-kubectl apply -f vault-auth-secret.yaml
